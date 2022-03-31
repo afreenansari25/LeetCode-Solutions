67619b478968c72cc51f58f3e3668bd32569756d
Afreen Ansari <100082758+afreenansari25@users.noreply.github.com>
2022-03-31 16:51:10 +0530

    Time: 429 ms (27.06%), Space: 25.5 MB (32.13%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -3,8 +3,29 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        """
         for i in range(k):
             temp = nums.pop(-1)
             nums.insert(0, temp)
         return nums
+        """
+        
+        k = k % len(nums)
+        l, r = 0, len(nums)-1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l+1, r-1
+        
+        l, r = 0, k-1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l+1, r-1
+            
+        l,r = k, len(nums)-1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l+1, r-1
+            
+        return nums
+    
         
