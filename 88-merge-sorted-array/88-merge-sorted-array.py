65542afd59e2249ab936bdbdb92e0fea96b09d7a
Afreen Ansari <100082758+afreenansari25@users.noreply.github.com>
2022-04-01 09:12:46 +0530

    Time: 52 ms (53.61%), Space: 14.1 MB (7.11%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -15,7 +15,8 @@ class Solution:
                 nums1[last] = nums2[n-1]
                 n -= 1
             last -= 1
-            
+        
+        # if nums1 has no elements 
         while n > 0:
             nums1[last] = nums2[n-1]
             n, last = n-1, last-1
