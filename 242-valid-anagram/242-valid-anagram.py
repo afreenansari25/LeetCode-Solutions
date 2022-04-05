7e2c01cd945f6947dbddf5cab437a14a98ce6417
Afreen Ansari <100082758+afreenansari25@users.noreply.github.com>
2022-04-05 12:08:42 +0530

    Time: 89 ms (23.55%), Space: 14.5 MB (69.83%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -2,13 +2,29 @@ from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         ''' 
+        # solution 1
         if Counter(s) == Counter(t):
             return True
         return False
-        '''
         
+        #solution 2
         if sorted(s) == sorted(t):
             return True
         return False
+        '''
+        countS, countT = {},{}
+        if len(s) != len(t):
+            return False
+        
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        for key in countS:
+            if countS[key] != countT.get(key,0):
+                return False
+        return True
+        
+        
+        
         
                 
