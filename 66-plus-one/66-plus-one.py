f9d23ace10242ada6140967c358aebeea6249902
Afreen Ansari <100082758+afreenansari25@users.noreply.github.com>
2022-04-10 15:37:57 +0530

    Time: 46 ms (51.46%), Space: 13.9 MB (63.44%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,10 +1,28 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        """
         n = ''.join(map(str, digits))
         m = int(n) + 1
         res = []
         for i in str(m):
             res.append(i)
         return res
-        #for i in range(len(digits)-1, 0):
+        
+        """
+        digits = digits[::-1]
+        carry, i = 1, 0
+        
+        while carry:
+            if i < len(digits):
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    digits[i] += 1
+                    carry = 0
+            else:
+                digits.append(1)
+                carry = 0
+            i += 1
+        return digits[::-1]
+        
             
