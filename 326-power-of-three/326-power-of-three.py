d0915e9b6dafda4ef7b6da6ea00776c95c311892
Afreen Ansari <100082758+afreenansari25@users.noreply.github.com>
2022-05-08 19:53:41 +0530

    Time: 108 ms (55.89%), Space: 13.7 MB (96.92%) - LeetHub

--- 326-power-of-three/326-power-of-three.py
+++ 326-power-of-three/326-power-of-three.py
@@ -4,10 +4,8 @@ class Solution:
             return False
         if n == 1:
             return True 
-        
+        if n%3 == 0:
+            return self.isPowerOfThree(n//3)
         else:
-            if n%3 == 0:
-                return self.isPowerOfThree(n//3)
-            else:
-                return False
+            return False
         
